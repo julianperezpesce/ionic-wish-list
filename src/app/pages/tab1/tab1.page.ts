029490d4247862b12247d3a6ae7be873ef3539d9
julian <estrotococoa@gmail.com>
2020-08-26 11:01:10 -0300

    new method addList() add.page.ts and implementation (click) add.page.html

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WishesService } from 'src/app/services/wishes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,12 @@ import { WishesService } from 'src/app/services/wishes.service';
 })
 export class Tab1Page {
 
-  constructor( public wishesService: WishesService) {
+  constructor( public wishesService: WishesService,
+               private router: Router) {
 
+  }
+
+  addList() {
+    this.router.navigateByUrl('tabs/add');
   }
 }
