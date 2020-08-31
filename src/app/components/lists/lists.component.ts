bb880a307dab637799cce53bc881b3643f0b2ee1
julian <estrotococoa@gmail.com>
2020-08-30 21:52:37 -0300

    sliding button now auto close after edit or cancel, made with @ViewChild

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { List } from 'src/app/models/list.model';
 import { Router } from '@angular/router';
 import { WishesService } from 'src/app/services/wishes.service';
+import { AlertController, IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-lists',
@@ -10,10 +11,12 @@ import { WishesService } from 'src/app/services/wishes.service';
 })
 export class ListsComponent implements OnInit {
 
+  @ViewChild(IonItemSliding) slidingItem: IonItemSliding;
   @Input() completeTask = true;
 
   constructor(public wishesService: WishesService,
-              private router: Router) { }
+              private router: Router,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
@@ -30,4 +33,48 @@ export class ListsComponent implements OnInit {
     this.wishesService.deleteList( list );
   }
 
+  async listEdit( list: List ) {
+    const alert = await this.alertCtrl.create({
+      cssClass: 'my-custom-class',
+      header: 'Editar lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          value: list.title,
+          placeholder: 'Nombre de la lista'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar'); 
+            
+             //this close the slide button edit - create
+            this.slidingItem.closeOpened();           
+          }
+        },
+        {
+          text: 'Actualizar lista',
+          handler: ( data ) => {
+            
+            // Validate list
+            if ( data.titulo.lenght === 0) {
+              return;
+            }
+            // edit list            
+            list.title = data.titulo;
+            this.wishesService.saveStorage();
+
+            //this close the slide button edit - create
+            this.slidingItem.closeOpened();
+          }
+        }
+      ]
+    });
+    
+    alert.present()
+  }
 }
